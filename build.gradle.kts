d4b9a8ed1d663b72287776a0e821103b7cb9a62f
pyeongho <pyeongho@dramancompany.com>
2024-07-18 18:46:08 +0900

    - 최초

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+subprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+
+    group = "com.pyeongho.hello"
+    version = "1.0.0"
+
+    configure<PublishingExtension> {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/pyeongho/hello")
+                credentials {
+                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                }
+            }
+        }
+        publications {
+            register<MavenPublication>("gpr") {
+                from(components["java"])
+            }
+        }
+    }
+
+    // jar task - manifest 파일에 라이브러리 이름과 버전을 포함  
+    tasks.jar {
+        manifest {
+            attributes(mapOf("Implementation-Title" to project.name,
+                "Implementation-Version" to project.version))
+        }
+    }
+}
