07e43c40eb156a3f33cf10859505aacf0fe55d60
pyeongho <pyeongho@dramancompany.com>
2024-07-19 13:02:10 +0900

    version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ subprojects {
     apply(plugin = "maven-publish")
 
     group = "com.pyeongho.hello"
-    version = "1.0.0"
+    version = "1.0.1"
 
     configure<PublishingExtension> {
         repositories {
